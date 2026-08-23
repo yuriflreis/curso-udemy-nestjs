77202c9a8cefa8109544456a2be8431600e95432
Yuri Reis <yuriflr@hotmail.com>
2026-08-23 10:01:17 -0400

    Fontes iniciais nestjs

---
+++ src/recados/recados.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+
+@Controller('recados')
+export class RecadosController {
+  @Get()
+  findAll() {
+    return 'Essa rota retorna todos os recados'
+  }
+  
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `Essa rota retorna o recado #${id}`
+  }
+
+  @Post()
+  create(@Body() body: any) {
+    return body;
+  }
+}
